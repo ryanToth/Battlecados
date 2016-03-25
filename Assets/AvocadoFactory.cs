daf91d3152883f79f85eaebef835f19178296857
Ryan Toth <ryan.toth@live.com>
2016-03-24 20:15:12 -0400

    Added the Avocado Factory

---
+++ Assets/AvocadoFactory.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+public class AvocadoFactory { 
+
+    public static Avocado GenerateEnemy(int storyLevel)
+    {
+        // Some equation to calculate the enemy's level
+        int level = storyLevel * 8 / 5 + 2;
+        // Gonna need a way to generate all of the enemy's equipped cards
+        Avocado avocado = new Avocado(storyLevel, 0, null, null, null, null, null, null, null, null);
+
+        return avocado;
+    }
+
+
+}
