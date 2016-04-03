1fab4311512716b8ee14ee7c4dcaa80752c993c3
Ryan Toth <ryan.toth@live.com>
2016-04-03 00:47:03 -0400

    View collection screen does some stuff, cells are clickable but dont do anything yet

---
+++ Assets/CardCollection/Scripts/CardList.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class CardList : MonoBehaviour {
+
+    User user;
+    public GameObject itemPrefab;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        user = GetCurrentInfo.User;
+        int max = user.Cards.Count;
+
+        for (int i = 0; i < max; i++)
+        {
+            //create a new item, name it, and set the parent
+            GameObject newItem = Instantiate(itemPrefab) as GameObject;
+
+            newItem.name = i.ToString();
+            var labels = newItem.GetComponentsInChildren<Text>();
+            var image = newItem.GetComponentsInChildren<Image>();
+
+            //Put cardID here cards[currentIndex].CardID.ToString()
+            image[1].sprite = Resources.Load("Cards/" + user.Cards[i].CardID.ToString(), typeof(Sprite)) as Sprite;
+
+            labels[0].text = user.Cards[i].Name;
+            //Attack
+            labels[6].text = user.Cards[i].AttackEffect.ToString();
+            //Defence
+            labels[7].text = user.Cards[i].DefenceEffect.ToString();
+            //Health
+            labels[8].text = user.Cards[i].HealthEffect.ToString();
+            //Speed
+            labels[9].text = user.Cards[i].SpeedEffect.ToString();
+            //Salvage Value
+            labels[10].text = user.Cards[i].SalvageValue.ToString();
+
+            newItem.transform.parent = gameObject.transform;
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
---
+++ Assets/CardCollection/Scripts/CardTile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardTile : MonoBehaviour {
+
+    public bool ReturnTrue()
+    {
+        return true;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
